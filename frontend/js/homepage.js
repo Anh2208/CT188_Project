003251f049ee75eeb14194fd83f5973f17917c1f
anhb1906425 <anhb1906425@student.ctu.edu.vn>
2023-03-31 19:10:38 +0700

    insert assets

---
+++ frontend/js/homepage.js
@@ -0,0 +1,31 @@
+import Img01 from './images/img01.jpg'
+
+const products = [
+
+    {
+        id: "01",
+        type: "thực phẩm",
+        mass: 500,
+        ingredient: [
+            {
+                nameIn: "mì chính" 
+            },
+            {
+                nameIn: "chính mì"
+            }
+        ],
+        use: "nướng ăn",
+        preserve: "bảo quản trong bụng",
+        trademark: "Mixfood",
+        production: "Việt Nam",
+        desc: "Đây là một sản phẩm phù hợp cho fast food",
+        manufacture: 22/8/2000,
+        expiry: 09/09/9999,
+        photo: Img01,
+    },
+    
+];
+    
+
+
+export default products
