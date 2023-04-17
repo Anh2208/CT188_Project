fb7df5559e81d983005d8856876fbb6ff4f6cc74
halleon2002 <82798557+halleon2002@users.noreply.github.com>
2023-04-17 22:33:48 +0700

    modify register, add register.js

---
+++ frontend/js/Register.js
@@ -0,0 +1,57 @@
+
+function Validator(options) {
+    //kiểm tra nhập thông tin
+    function validate(inputElement, rule) {
+        var errorMessage = rule.test(inputElement.value);
+        var errorElement = inputElement.parentElement.querySelector('.form-message');
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid');
+        }
+
+    }
+
+    var formElement = document.querySelector(options.form);
+
+    if (formElement) {
+        options.rules.forEach(function (rule) {
+
+            var inputElement = formElement.querySelector(rule.selector);
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+
+                }
+            }
+
+        })
+
+    }
+
+}
+
+Validator.isRequired = function (selector) {
+    return {
+        selector: selector,
+        test: function (value) {
+
+            return value.trim() ? undefined : 'Vui lòng nhập tên'
+        }
+    }
+}
+
+
+Validator.isEmail = function (selector) {
+    return {
+        selector: selector,
+        test: function () {
+
+        }
+    }
+
+}
+
