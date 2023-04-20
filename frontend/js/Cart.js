306adef093e0a29c5b13fbc5938a5d00f185af20
halleon2002 <82798557+halleon2002@users.noreply.github.com>
2023-04-20 16:28:53 +0700

    modify cart

---
+++ frontend/js/Cart.js
@@ -0,0 +1,26 @@
+// xóa cart
+var remove_cart = document.getElementsByClassName("btn-danger");
+for (var i = 0; i < remove_cart.length; i++) {
+    var button = remove_cart[i]
+    button.addEventListener("click", function () {
+        var button_remove = event.target
+        button_remove.parentElement.parentElement.remove()
+    })
+}
+
+//update cart
+function updateCart() {
+    var cart_item = document.getElementsByClassName("cart-items")[0];
+    var cart_rows = cart_item.getElementsByClassName("cart-rows");
+    var total = 0;
+    var i = 0;
+    while (i < cart_rows.length) {
+        var cart_row = cart_rows[i];
+        var price_item = cart_row.getElementsByClassName("price_item")[0]
+        var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0]
+        var price = parseFloat(price_item.innerText);
+        var quantity = quantity_item.value;
+        total += (price * quantity);
+    }
+    document.getElementsByClassName("cart-total-price")[0].innerText = total + 'VNĐ'
+}
