876c9e77dd746e0dee381b6e366466d1877945b7
halleon2002 <82798557+halleon2002@users.noreply.github.com>
2023-05-14 14:24:13 +0700

    admin/product

--- admin/js/addproduct.js
+++ admin/js/addproduct.js
@@ -1,9 +1,323 @@
+let products = [
+    {
+        id: 1,
+        name: "6 lon nước ngọt Coca Cola 320ml",
+        image: "../../frontend/assets/images/coca-1.jpg",
+        price: 58600,
+        type: "Nước giải khát",
+    },
+
+    {
+        id: 2,
+        name: "Thùng 30 gói mì Hảo Hảo tôm chua cay 75g",
+        image:
+            "../../frontend/assets/images/thung-30-goi-mi-hao-hao-tom-chua-cay-75g-202211181144401243.jpg",
+        price: 120000,
+        type: "Mì",
+    },
+    {
+        id: 3,
+        name: "Quýt giống Úc 1kg (5 - 9 trái)",
+        image: "../../frontend/assets/images/quytut1.jpg",
+        price: 30000,
+        type: "Trái cây",
+    },
+    {
+        id: 4,
+        name: "Thùng 30 gói mì Đệ Nhất thịt bằm 82g",
+        image: "../../frontend/assets/images/mydenhat1.jpg",
+        price: 241000,
+        type: "Mì",
+    },
+    {
+        id: 5,
+        name: "Rượu soju Good Day vị đào 13.5% chai 360ml",
+        image: "../../frontend/assets/images/soju1.jpg",
+        price: 63000,
+        type: "Nước giải khát",
+    },
+    {
+        id: 6,
+        name: "Thùng 24 lon bia Heineken Sleek 330ml",
+        image: "../../frontend/assets/images/heniken1.jpg",
+        price: 430000,
+        type: "Nước giải khát",
+    },
+    {
+        id: 7,
+        name: "Thùng 24 ly mì Modern lẩu Thái tôm 65g",
+        image: "../../frontend/assets/images/mylauthai1.jpg",
+        price: 180000,
+        type: "Mì",
+    },
+    {
+        id: 8,
+        name: "Quýt giống Úc 1kg (5 - 9 trái)",
+        image: "../../frontend/assets/images/quytut1.jpg",
+        price: 62000,
+        type: "Trái cây",
+    },
+    {
+        id: 9,
+        name: "Thùng 30 gói mì Kokomi 90 tôm chua cay 90g",
+        image: "../../frontend/assets/images/kokomi1.jpg",
+        price: 180000,
+        type: "Mì",
+    },
+    {
+        id: 10,
+        name: "Nấm bào ngư trắng 300g",
+        image: "../../frontend/assets/images/nam-bao-ngu-1.jpg",
+        price: 18000,
+        type: "Rau củ",
+    },
+    {
+        id: 11,
+        name: "Nấm kim châm Hàn Quốc 150g",
+        image: "../../frontend/assets/images/nam-kim-cham-1.jpg",
+        price: 13900,
+        type: "Rau củ"
+    },
+    {
+        id: 12,
+        name: "Khoai mỡ 1kg (1 - 2 củ)",
+        image: "../../frontend/assets/images/khoai-mo-tui-1.jpg",
+        price: 37800,
+        type: "Rau củ",
+    },
+    {
+        id: 13,
+        name: "Khổ qua 500g (3 - 5 trái)",
+        image: "../../frontend/assets/images/kho-qua-1.jpg",
+        price: 16300,
+        type: "Rau củ",
+    },
+    {
+        id: 14,
+        name: "Cà chua 500g (7-9 trái)",
+        image: "../../frontend/assets/images/ca-chua-1.jpg",
+        price: 12000,
+        type: "Rau củ",
+    },
+    {
+        id: 15,
+        name: "Dưa hấu ruột đỏ túi 2kg",
+        image: "../../frontend/assets/images/dua-hau1.jpg",
+        price: 35200,
+        type: "Trái cây",
+    },
+    {
+        id: 16,
+        name: "Thùng 12 lon bia Tiger 500ml",
+        image: "../../frontend/assets/images/tiger-1.jpg",
+        price: 210000,
+        type: "Nước giải khát",
+    },
+    {
+        id: 17,
+        name: "Thùng 30 gói hủ tiếu Nam Vang Nhịp Sống 70g",
+        image: "../../frontend/assets/images/hu-tieu-1.jpg",
+        price: 240000,
+        type: "Hủ tiếu",
+    },
+    {
+        id: 18,
+        name: "Thùng 24 gói miến sườn heo Vifon 58g",
+        image: "../../frontend/assets/images/mien-suon1.jpg",
+        price: 247000,
+        type: "Miến",
+    },
+
+    {
+        id: 19,
+        name: "Táo mật Koru Mỹ 1kg (4 - 5 trái)",
+        image: "../../frontend/assets/images/tao-koru-1.jpg",
+        price: 59000,
+        type: "Trái cây",
+    },
+    {
+        id: 20,
+        name: "Thùng 20 lon bia Budweiser 330ml",
+        image: "../../frontend/assets/images/budweiser1.jpg",
+        price: 290000,
+        type: "Nước giải khát",
+    },
+    {
+        id: 21,
+        name: "5 tô hủ tiếu Nam Vang Nhớ mãi mãi tô 71g",
+        image: "../../frontend/assets/images/combo-5-to-hu-tieu1.jpg",
+        price: 81500,
+        type: "Hủ tiếu",
+    },
+    {
+        id: 22,
+        name: "Thùng 24 gói miến sườn heo Vifon 58g",
+        image: "../../frontend/assets/images/10-goi-mien-1.jpg",
+        price: 113000,
+        type: "Miến",
+    },
+
+    {
+        id: 23,
+        name: "Ổi nữ hoàng 1kg (3-5 trái)",
+        image: "../../frontend/assets/images/oi1.jpg",
+        price: 19300,
+        type: "Trái cây",
+    },
+    {
+        id: 24,
+        name: "Thùng 24 lon Strongbow dâu đen 330ml",
+        image: "../../frontend/assets/images/strongbow1.jpg",
+        price: 39000,
+        type: "Nước giải khát",
+    },
+    {
+        id: 25,
+        name: "Kim chi cải thảo cắt lát Bibigo Ông Kim hộp 500g",
+        image: "../../frontend/assets/images/kim-chi1.jpg",
+        price: 47500,
+        type: "Thực phẩm",
+    },
+    {
+        id: 26,
+        name: "Rau quả hỗn hợp đông lạnh SG Food gói 500g",
+        image: "../../frontend/assets/images/rau-qua-1.jpg",
+        price: 42000,
+        type: "Rau củ",
+    },
+    {
+        id: 27,
+        name: "Bắp hạt đông lạnh Mama Food gói 250g",
+        image: "../../frontend/assets/images/bap-hat-1.jpg",
+        price: 18000,
+        type: "Rau củ",
+    },
+    {
+        id: 28,
+        name: "Me chua 68g",
+        image: "../../frontend/assets/images/me-chua-1.jpg",
+        price: 4500,
+        type: "Trái cây",
+    },
+    {
+        id: 29,
+        name: "Thùng 30 gói phở bò Vifon 65g",
+        image: "../../frontend/assets/images/pho-bo-1.jpg",
+        price: 210000,
+        type: "Phở",
+    },
+    {
+        id: 30,
+        name: "10 gói bún mắm Hằng Nga 75g",
+        image: "../../frontend/assets/images/bun-mam-1.jpg",
+        price: 99000,
+        type: "Bún",
+    },
+    {
+        id: 31,
+        name: "5 gói phở gà Vifon 65g",
+        image: "../../frontend/assets/images/pho-ga-1.jpg",
+        price: 42500,
+        type: "Phở",
+    },
+    {
+        id: 32,
+        name: "10 gói bún bò huế Hằng Nga 73g",
+        image: "../../frontend/assets/images/bun-bo-1.jpg",
+        price: 95000,
+        type: "Bún",
+    },
+    {
+        id: 33,
+        name: "Thùng 48 hộp sữa tươi tiệt trùng có đường Kun 180ml",
+        image: "../../frontend/assets/images/sua-1.jpg",
+        price: 310000,
+        type: "Sữa",
+    },
+    {
+        id: 34,
+        name: "Sữa đặc có đường Ngôi sao Phương Nam Xanh lá hộp 380g",
+        image: "../../frontend/assets/images/sua-dac-1.jpg",
+        price: 20600,
+        type: "Sữa",
+    },
+    {
+        id: 35,
+        name: "Ngũ cốc dinh dưỡng VinaCafé Bfast bịch 500g",
+        image: "../../frontend/assets/images/ngu-coc-1.jpg",
+        price: 67800,
+        type: "Ngủ cốc",
+    },
+    {
+        id: 36,
+        name: "Bánh Malkist Crackers Roma gói 216g",
+        image: "../../frontend/assets/images/banh-quy-1.jpg",
+        price: 19500,
+        type: "Bánh",
+    },
+    {
+        id: 37,
+        name: "2 gói snack khoai tây vị kim chi Hàn Quốc OStar 108g",
+        image: "../../frontend/assets/images/snack-1.jpg",
+        price: 45800,
+        type: "Bánh",
+    },
+    {
+        id: 38,
+        name: "Đậu phộng sả ớt Tài Tài hũ 250g",
+        image: "../../frontend/assets/images/dau-phong-1.jpg",
+        price: 43000,
+        type: "Bánh",
+    },
+];
+function showProduct() {
+    table = `<tr>
+       <th>ID</th>
+       <th>Tên sản phẩm</th>
+       <th>Loại</th>
+       <th>Giá</th>
+       <th>Hình ảnh</th>
+       <th>Chỉnh sửa</th>
+   </tr>`
+    for (let i = 0; i < products.length; ++i) {
+        table += `<tr>
+        
+       <td>${products[i].id}</td>
+       <td>${products[i].name}</td>
+       <td>${products[i].type}</td>
+       <td>${products[i].price}</td>
+       <td><img width ="50px" src="${products[i].image}"></td>
+       <td>
+            <button onclick="editItem(${products[i].id})"><i class="fa fa-pencil"></i></button>
+            <button onclick="deleteItem(${products[i].id})"><i class="fa fa-trash-can"></i></button>
+        </td>
+   </tr>`
+    }
+    var listItem = localStorage.getItem("list-item") ? JSON.parse(localStorage.getItem("list-item")) : []
+    for (let i = 0; i < listItem.length; i++) {
+        table += `<tr>
+        <td>${listItem[i].id}</td>
+        <td>${listItem[i].name}</td>
+        <td>${listItem[i].type}</td>
+        <td>${listItem[i].price}</td>            
+        <td><img src=""></td>
+        <td>
+            <button onclick="editItem(${listItem[i].id})"><i class="fa fa-pencil"></i></button>
+            <button onclick="deleteItem(${listItem[i].id})"><i class="fa fa-trash-can"></i></button>
+        </td>
+    </tr>`
+    }
+    document.getElementById("itemList").innerHTML = table
+
+}
+
+showProduct()
+
 function checkID(id) {
     let listItem = localStorage.getItem("list-item") ? JSON.parse(localStorage.getItem("list-item")) : []
 
     for (let i = 0; i < listItem.length; i++) {
         if (listItem[i].id == id) {
-            console.log(listItem[i].id)
             return true;
         }
     }
@@ -16,21 +330,17 @@ function add() {
     var name = document.getElementById("name").value
     var type = document.getElementById("type").value
     var price = document.getElementById("price").value
-    var quantity = document.getElementById("quantity").value
     var img = document.getElementById("img").value
-
     let item = localStorage.getItem("list-item") ? JSON.parse(localStorage.getItem("list-item")) : []
     item.push({
         id: id,
         name: name,
         type: type,
         price: price,
-        quantity: quantity,
         img: img
     })
 
     localStorage.setItem("list-item", JSON.stringify(item))
-    console.log(localStorage)
     if (!checkID(id)) {
         alert("Sản phẩm đã tồn tại! Vui lòng nhập sản phẩm khác")
     }
@@ -73,48 +383,12 @@ function add() {
 //}
 
 
-
-//hiển thị Item
-var render = (listItem) => {
-    listItem = localStorage.getItem("list-item") ? JSON.parse(localStorage.getItem("list-item")) : []
-    table = `<tr>
-                <th>ID</th>
-                <th>Tên sản phẩm</th>
-                <th>Loại</th>
-                <th>Giá</th>
-                <th>Số lượng</th>
-                <th>Hình ảnh</th>
-                <th>Chỉnh sửa</th>
-                </tr>`;
-    for (let i = 0; i < listItem.length; i++) {
-        table += `<tr>
-        <td>${listItem[i].id}</td>
-        <td>${listItem[i].name}</td>
-        <td>${listItem[i].type}</td>
-        <td>${listItem[i].price}</td>
-        <td>${listItem[i].quantity}</td>            
-        <td><img src=""></td>
-        <td>
-            <button onclick="editItem(${listItem[i].id})"><i class="fa fa-pencil"></i></button>
-            <button onclick="deleteItem(${listItem[i].id})"><i class="fa fa-trash-can"></i></button>
-        </td>
-    </tr>`
-        document.getElementById('itemList').innerHTML = table;
-    }
-
-
-}
-
-render()
-
-
 function clear() {
     document.getElementById("id").value = ""
     document.getElementById("name").value = ""
     document.getElementById("type").value = ""
     document.getElementById("price").value = ""
-    document.getElementById("quantity").value = ""
-    document.getElementById("img").value = ""
+    document.getElementById("img").input = ""
 }
 
 //sửa Item
@@ -134,14 +408,23 @@ function clear() {
 //xoá Item
 function deleteItem(x) {
     let listItem = localStorage.getItem("list-item") ? JSON.parse(localStorage.getItem("list-item")) : []
+
     for (let i = 0; i < listItem.length; i++) {
         if (listItem[i].id == x) {
             listItem.splice(i, 1)
             localStorage.setItem("list-item", JSON.stringify(listItem))
-            render()
+            showProduct()
+        }
+    }
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id == x) {
+            products.splice(i, 1)
+            localStorage.setItem("list-item", JSON.stringify(products))
+            showProduct()
         }
     }
 }
+
 //tìm kiếm Item
 function searchItem() {
     let input = document.getElementById("searchInput").value;
@@ -177,9 +460,43 @@ function searchItem() {
                                     <td>${result[i].name}</td>
                                     <td>${result[i].type}</td>
                                     <td>${result[i].price}</td>
-                                    <td>${result[i].quantity}</td>
-                                    <td><img src=""></td>
+                                    <td><img src="${result[i].img}"></td>
                                 </tr>`
+                    document.getElementById("tableSearch").innerHTML = table
+                    console.log(result)
+                }
+            }
+        }
+        //tìm cho gán cứng
+        for (let i = 0; i < products.length; i++) {
+            var str = products[i].name
+            if (str.includes(input)) {
+                document.getElementById("tableSearch").style.display = "block";
+                document.getElementById("itemList").style.display = "none";
+                var itemSearch = {
+                    id: products[i].id,
+                    name: products[i].name,
+                    type: products[i].type,
+                    price: products[i].price,
+                    image: products[i].image
+                }
+                result.push(itemSearch)
+
+                table = `<tr>
+                                <th>ID</th>
+                                <th>Tên sản phẩm</th>
+                                <th>Loại</th>
+                                <th>Giá</th>
+                                <th>Hình ảnh</th>
+                            </tr>`;
+                for (let i = 0; i < result.length; i++) {
+                    table += `<tr>
+                                <td>${result[i].id}</td>
+                                <td>${result[i].name}</td>
+                                <td>${result[i].type}</td>
+                                <td>${result[i].price}</td>                                    
+                                <td><img src="${result[i].image}"></td>
+                            </tr>`
                     document.getElementById("tableSearch").innerHTML = table
                     console.log(result)
                 }
